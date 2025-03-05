0d8286fda038887151f5b46d62017a0b5809c019
Hector Matus <hmatus.dev@gmail.com>
2025-03-05 15:48:19 -0600

    feat: Added swagger endpoint documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  patchNestJsSwagger();
+  const config = new DocumentBuilder()
+    .setTitle('Boxful API')
+    .setDescription('Boxful API for test application')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(3000);
   const server = app.getHttpServer();
   const address = server.address();
