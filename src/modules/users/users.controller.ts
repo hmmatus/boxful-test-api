754f20020999a0e73d44b2b12326278efa5f1c6c
Hector Matus <hmatus.dev@gmail.com>
2025-03-05 14:59:39 -0600

    Fix: Added logs to login endpoint, removed unused usepipes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ZodValidationPipe } from '@/pipes/zod-validation.pipe';
 import {
@@ -23,7 +30,6 @@ export class UsersController {
   async login(@Body() loginUserDTO: LoginUserDTO) {
     return this.usersService.login(loginUserDTO);
   }
-  @UsePipes()
   @Post('register')
   @UsePipes(new ZodValidationPipe(CreateUserSchema))
   @UsePipes(EmailExistsPipe)
