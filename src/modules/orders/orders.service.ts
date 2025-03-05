d089148bc32977b6f79b65e4aff5e8d2e8ba896e
Hector Matus <hmatus.dev@gmail.com>
2025-03-04 21:16:17 -0600

    feat: Added orders service get and create, add zod as type validator

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Order, Prisma } from '@prisma/client';
+import { CreateOrderDTO } from '@/types/schemas/order.schema';
 
 @Injectable()
 export class OrdersService {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createOrder(data: CreateOrderDTO): Promise<Order> {
+    const formattedData: Prisma.OrderCreateInput = {
+      recolectionAddress: data.recolectionAddress,
+      scheduledDate: data.scheduledDate,
+      names: data.names,
+      lastNames: data.lastNames,
+      email: data.email,
+      phone: data.phone,
+      destinyAddress: data.destinyAddress,
+      department: data.department,
+      municipality: data.municipality,
+      reference: data.reference,
+      notes: data.notes,
+      lumps: {
+        create: data.lumps.map((lump) => ({
+          length: lump.length,
+          width: lump.width,
+          height: lump.height,
+          weight: lump.weight,
+          content: lump.content,
+        })),
+      },
+    };
+    return this.prismaService.order.create({ data: formattedData });
+  }
+
+  async getAllOrders(): Promise<Order[]> {
+    return this.prismaService.order.findMany();
+  }
 }
---
+++ src/pipes/zod-validation.pipe.ts
@@ -0,0 +1,30 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        // Format Zod errors into a more user-friendly format
+        const formattedErrors = error.errors.map((err) => ({
+          field: err.path.join('.'),
+          message: err.message,
+        }));
+        throw new BadRequestException({
+          message: 'Validation failed',
+          errors: formattedErrors,
+        });
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
