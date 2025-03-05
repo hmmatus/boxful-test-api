4d2f6728251975b2a7b93011756962d10328d2f7
Hector Matus <hmatus.dev@gmail.com>
2025-03-04 18:57:33 -0600

    feat: Added config file, orders module and first prisma migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+const { NODE_ENV, PORT, DATABASE_URL } = process.env;
+
+export default { NODE_ENV, PORT, DATABASE_URL };
